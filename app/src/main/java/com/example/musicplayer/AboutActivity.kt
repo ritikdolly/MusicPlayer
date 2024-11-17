7b6dd9d977d25177b8ef3f6f74f44d31caccba0c
Ritik Kumar <ritikkumartulbul83753@gmail.com>
2024-11-17 17:58:18 +0530

    add favourite_view , FeedbackActivity ,musicviews page

---
+++ app/src/main/java/com/example/musicplayer/AboutActivity.kt
@@ -0,0 +1,29 @@
+package com.example.musicplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.harshRajpurohit.musicPlayer.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(MainActivity.currentThemeNav[MainActivity.themeIndex])
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = "About"
+        binding.aboutText.text = aboutText()
+    }
+
+    private fun aboutText(): String {
+        return """:
+            1. Sahdev (Registration No: 220301120403)
+            2. Ritik (Registration No: 220301120409)
+            3. Debasis (Registration No: 220301120409)
+
+            If you want to provide feedback, I will love to hear that.
+        """.trimIndent()
+    }
+}
